aec42bf78209deb65da7303c8b5ae03fe511a6f2
kadicha07 <bakytbekovakadicha@gmail.com>
2024-04-15 15:46:51 +0600

    kadicha first commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Intro from "../components/Intro";
+import Frame from "../components/Frame";
 
 function HomePage() {
   return (
     <>
       <Intro />
+      <Frame />
     </>
   );
 }
